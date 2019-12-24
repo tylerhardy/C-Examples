eb83887c20fab4071d89d07fe6fc907f66425595
tylerhardy <tylerhardy@gmail.com>
2019-12-24 10:57:13 -0700

    init

--- C++Examples/main.cpp
+++ C++Examples/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+struct User
+{
+public:
+	std::string first_name;
+	std::string last_name;
+	std::string get_status()
+	{
+		return status;
+	}
+private:
+	std::string status = "Gold";
+};
+
 
 int main() 
 {
-	std::cout << "Hello World!" << std::endl;
+	User me;
+	me.first_name = "Tyler";
+	me.last_name = "Hardy";
+	//me.status = "Gold";
+	std::cout << "First name: " << me.first_name << "Status: " << me.get_status() << std::endl;
 
 	return 0;
 }
